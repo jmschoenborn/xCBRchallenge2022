12be3339a6ae09095d5bf07ee110ae0e62bdf6e9
Jakob Schoenborn <schoenb@uni-hildesheim.de>
2022-07-19 12:42:54 +0200

    Adjust example output message.

--- jms.git.xCBRchallenge2022/src/main/java/jms/git/xCBRchallenge2022/GitTest.java
+++ jms.git.xCBRchallenge2022/src/main/java/jms/git/xCBRchallenge2022/GitTest.java
@@ -2,6 +2,6 @@ package jms.git.xCBRchallenge2022;
 
 public class GitTest {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World! Another change.");
 	}
 }
